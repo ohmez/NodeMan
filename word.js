e088c09e7096c8bf0a2eba95bce697fb0106d8d2
ohmez <41805219+ohmez@users.noreply.github.com>
2018-11-02 16:42:14 -0600

    format fix

--- word.js
+++ word.js
@@ -5,23 +5,26 @@ this.word = word;
 this.lettersArr = word.split("");
 this.created = [];
 this.create = function () {
-for (x=0; x<this.lettersArr.length; x++) {
-this.created.push(new Letter(this.lettersArr[x]));
-}
-  }; // end create function/key.
+	for (x=0; x<this.lettersArr.length; x++) {
+	this.created.push(new Letter(this.lettersArr[x]));
+	}
+}; // end create function/key.
+	
 this.populate = function() {
 	var pop = [];
-for (x=0; x<this.created.length; x++) {
-pop.push(this.created[x].return());
+	for (x=0; x<this.created.length; x++) {
+	pop.push(this.created[x].return());
 	};// end loop
-console.log(pop.join(" "));
+	console.log(pop.join(" "));
 };// end populate function/key
+	
 this.checker = function (guess) {
-for (x=0; x<this.created.length; x++) { 
-this.created[x].check(guess);
-}
-this.populate();
+	for (x=0; x<this.created.length; x++) { 
+	this.created[x].check(guess);
+	}
+	this.populate();
 };// end checker function/key
+	
 this.create(); // runs the create function upon new Word(); 
 };// end Word function 
 
