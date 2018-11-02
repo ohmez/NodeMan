fe7ddc9daf09c17c6664577d98c9cb379fb7c1fc
ohmez <41805219+ohmez@users.noreply.github.com>
2018-11-02 16:40:24 -0600

    Tested and working Word() consructor
    wrote on WordPad, works when both Letter() and Word() are in same sheet, should work if i set up require() properly.

--- word.js
+++ word.js
@@ -2,12 +2,27 @@ var letter = require('./letter.js');
 
 function Word (word) {
 this.word = word;
-this.lettersArr = word.split();
+this.lettersArr = word.split("");
+this.created = [];
 this.create = function () {
-lettersArr.foreach(new letter());
-  new letter(); 
-  };
+for (x=0; x<this.lettersArr.length; x++) {
+this.created.push(new Letter(this.lettersArr[x]));
+}
+  }; // end create function/key.
+this.populate = function() {
+	var pop = [];
+for (x=0; x<this.created.length; x++) {
+pop.push(this.created[x].return());
+	};// end loop
+console.log(pop.join(" "));
+};// end populate function/key
+this.checker = function (guess) {
+for (x=0; x<this.created.length; x++) { 
+this.created[x].check(guess);
+}
+this.populate();
+};// end checker function/key
+this.create(); // runs the create function upon new Word(); 
+};// end Word function 
 
-};
-
-Word('bananas');
+var banana = new Word('bananas');
