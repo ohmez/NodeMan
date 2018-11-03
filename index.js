af8b283ea11e587fc774866c6bc4d89078e2eb31
ohmez <41805219+ohmez@users.noreply.github.com>
2018-11-02 20:08:43 -0600

    correct guesses counter initiated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var totalG = 10;
 var words = ['banana', 'naruto', 'league of legends'];
 var rando = Math.floor(Math.random() * words.length);
 var guess = new Word(words[rando]);
-var totalO = 0;
+var totalC = 0;
 for (x = 0; x < guess.created.length; x ++) {
   if(guess.created[x].value) {
 totalO += 1;}
@@ -13,5 +13,15 @@ totalO += 1;}
 
 inquirer.prompt([{type: 'input', message: 'Press any letter to start NodeMan', name: 'guess'}])
 .then(answers => {
+for (x = 0; x < guess.created.length; x ++) {
+  if(guess.created[x].value == answers.guess) {
+  totalC += 1;
+  }
+}
+  
+if(totalC <= 0) {
+  totalG -= 1; 
+  console.log(totalG + ' Guesses remaining.');
+}
   guess.checker(answers.guess);
 });
