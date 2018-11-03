9caac86e7f9ec01ce9834e1ebe44ce036beb56fc
ohmez <41805219+ohmez@users.noreply.github.com>
2018-11-02 18:15:26 -0600

    working for multi word inputs. I.E Jurassic Park

--- word.js
+++ word.js
@@ -1,26 +1,37 @@
 var letter = require('./letter.js');
 
 function Word (word) {
-this.word = word;
-this.lettersArr = word.split("");
+this.word = word.trim();
+console.log(this.word);
+this.lettersArr = this.word.split("");
+console.log(this.lettersArr);
 this.created = [];
 this.create = function () {
 	for (x=0; x<this.lettersArr.length; x++) {
-	this.created.push(new Letter(this.lettersArr[x]));
+	if(this.lettersArr[x] == ' ') {
+	this.created.push(' ');
+	} else {
+		this.created.push(new Letter(this.lettersArr[x]));
+		}
 	}
 }; // end create function/key.
 	
 this.populate = function() {
 	var pop = [];
 	for (x=0; x<this.created.length; x++) {
+	if(this.created[x] == ' ') {pop.push(' ');} else {
 	pop.push(this.created[x].return());
-	};// end loop
+	}
+	}// end loop
 	console.log(pop.join(" "));
 };// end populate function/key
 	
 this.checker = function (guess) {
 	for (x=0; x<this.created.length; x++) { 
-	this.created[x].check(guess);
+	if(this.created[x] == ' ') {
+	//do nothing
+	} else {
+	this.created[x].check(guess); }
 	}
 	this.populate();
 };// end checker function/key
@@ -28,4 +39,4 @@ this.checker = function (guess) {
 this.create(); // runs the create function upon new Word(); 
 };// end Word function 
 
-var banana = new Word('bananas');
+var banana = new Word('  banana cream pie  ');
