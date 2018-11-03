77fd4cf28d3e7835716b11dc0138ea5d9e3044ca
ohmez <41805219+ohmez@users.noreply.github.com>
2018-11-02 19:54:17 -0600

    starting index

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 var word = require('./word.js');
+var inquirer = require('inquirer');
 
+var totalG = 10;
 var words = ['banana', 'naruto', 'league of legends'];
+var rando = Math.floor(Math.random() * words.length);
+var guess = new Word(words[rando]);
+var totalO = 0;
+for (x = 0; x < guess.created.length; x ++) {
+  if(guess.created[x].value) {
+totalO += 1;}
+}
+
+inquirer.prompt([{type: 'input', message: 'Press any letter to start NodeMan', name: 'guess'}])
+.then(answers => {
+  guess.checker(answers.guess);
+});
